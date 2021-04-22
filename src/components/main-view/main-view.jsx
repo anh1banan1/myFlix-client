df3da53e2c9fd9e762f2981c01e2c548f7a832d1
anh1banan1 <72141667+anh1banan1@users.noreply.github.com>
2021-04-22 17:43:13 +0200

    Update main-view.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -6,6 +6,11 @@ import { RegistrationView } from '../registration-view/registration-view';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 
+import './main-view.scss';
+
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 export class MainView extends React.Component {
     
     constructor(){
@@ -61,14 +66,20 @@ export class MainView extends React.Component {
         if (movies.length === 0) return <div className="main-view" />;
 
         return (
-            <div className="main-view">
+            <Row className="main-view" justify-content-md-center>
                 {selectedMovie
-                    ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
+                    ? (
+                        <Col md={8}>
+                            <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
+                        </Col>
+                    )
                     : movies.map(movie => (
-                        <MovieCard key={movie._id} movie={movie} onMovieClick={(movie) => { this.setSelectedMovie(movie) }}/>
+                        <Col md={3}>
+                            <MovieCard key={movie._id} movie={movie} onMovieClick={(newSelectedMovie) => { this.setSelectedMovie(newSelectedMovie); }}/>
+                        </Col>
                     ))
                 }
-            </div>
-            );
+            </Row>
+        );
     }
 }
