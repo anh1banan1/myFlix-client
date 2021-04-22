b2480e88981223d5dadcb80104ea61c4ec1589b4
anh1banan1 <72141667+anh1banan1@users.noreply.github.com>
2021-04-22 14:42:46 +0200

    Update main-view.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
-// import axios from 'axios';
+import axios from 'axios';
 
 import { LoginView } from '../login-view/login-view';
+import { RegistrationView } from '../registration-view/registration-view',
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 
@@ -11,29 +12,24 @@ export class MainView extends React.Component {
         super();
         // Initial state is set to null
         this.state = {
-            movies: [
-                { _id: 1, Title: 'Silence of the Lambs', Description: 'A young FBI cadet must receive the help of an incarcerated and manipulative cannibal killer to help catch another serial killer.', Genre: 'Thriller',Director: 'Jonathan Demme', ImagePath: 'https://upload.wikimedia.org/wikipedia/en/8/86/The_Silence_of_the_Lambs_poster.jpg'},
-                { _id: 2, Title: 'Crazy Rich Asians', Description: 'Rachel, a professor, dates a man named Nick and looks forward to meeting his family. However, she is shaken up when she learns that Nick belongs to one of the richest families in the country.', Genre: 'Romance', Director: 'Jon M. Chu', ImagePath: 'https://upload.wikimedia.org/wikipedia/en/b/ba/Crazy_Rich_Asians_poster.png'},
-                { _id: 3, Title: 'Joker', Description: 'Arthur Fleck, a party clown, leads an impoverished life with his ailing mother. However, when society shuns him and brands him as a freak, he decides to embrace the life of crime and chaos.', Genre: 'Thriller', Director: 'Todd Phillips', ImagePath: 'https://upload.wikimedia.org/wikipedia/en/e/e1/Joker_%282019_film%29_poster.jpg'},
-                { _id: 4, Title: 'The Great Gatsby', Description: 'Nick Carraway, a World War I veteran who moves to New York with the hope of making it big, finds himself attracted to Jay Gatsby and his flamboyant lifestyle.', Genre: 'Drama', Director: 'Baz Luhrmann', ImagePath: 'https://upload.wikimedia.org/wikipedia/en/c/c2/TheGreatGatsby2013Poster.jpg'},
-                { _id: 5, Title: 'Knocked Up', Description: 'Alison, a TV Host, and Ben, a jobless man, end up having a one-night stand. Alison soon discovers that she is pregnant with Ben"s child and they must decide on their priorities and make a choice.', Genre: 'Comedy', Director: 'Judd Apatow', ImagePath: 'https://upload.wikimedia.org/wikipedia/en/5/51/Knockedupmp.jpg'}
-            ],
+            movies: [],
             selectedMovie: null,
-            user: null
+            user: null,
+            newRegistration: null
         };
     }
 
-    // componentDidMount(){
-    //     axios.get('https://mooflix.herokuapp.com/movies')
-    //         .then(response => {
-    //             this.setState({
-    //                 movies: response.data
-    //             });
-    //         })
-    //         .catch(error => {
-    //             console.log(error);
-    //         });
-    // }
+    componentDidMount(){
+        axios.get('https://mooflix.herokuapp.com/movies')
+            .then(response => {
+                this.setState({
+                    movies: response.data
+                });
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
 
     setSelectedMovie(movie) {
         this.setState({
@@ -47,11 +43,19 @@ export class MainView extends React.Component {
         });
     }
 
+    onRegister(newRegistration) {
+        this.setState({
+            newRegistration
+        });
+    }
+
     render() {
-        const { movies, selectedMovie } = this.state;
+        const { movies, selectedMovie, user, newRegistration } = this.state;
 
         // If there is no user, the LoginView is rendered. If there is a user logged in, the user details are *passed as a prop to the LoginView
         if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />; 
+
+        if (!newRegistration) return <RegistrationView onRegister={(newRegistration) => this.onRegister(newRegistration)} />;
 
         // Before the movies have been loaded
         if (movies.length === 0) return <div className="main-view" />;
@@ -61,7 +65,7 @@ export class MainView extends React.Component {
                 {selectedMovie
                     ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
                     : movies.map(movie => (
-                        <MovieCard key={movie._id} movie={movie} onMovieClick={(newSelectedMovie) => { this.setSelectedMovie(newSelectedMovie) }}/>
+                        <MovieCard key={movie._id} movie={movie} onMovieClick={(movie) => { this.setSelectedMovie(movie) }}/>
                     ))
                 }
             </div>
