db4756eb927b467ece6b179a030b1890f31c7397
anh1banan1 <72141667+anh1banan1@users.noreply.github.com>
2021-04-23 14:49:45 +0200

    Update main-view.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import axios from 'axios';
 
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 import { LoginView } from '../login-view/login-view';
 import { RegistrationView } from '../registration-view/registration-view';
 import { MovieCard } from '../movie-card/movie-card';
@@ -25,27 +27,45 @@ export class MainView extends React.Component {
     }
 
     componentDidMount(){
-        axios.get('https://mooflix.herokuapp.com/movies')
-            .then(response => {
-                this.setState({
-                    movies: response.data
-                });
-            })
-            .catch(error => {
-                console.log(error);
+        let accessToken = localStorage.getItem('token');
+        if (accessToken !== null) {
+            this.setState({
+                user: localStorage.getItem('user')
             });
+            this.getMovies(accessToken);
+        }
     }
 
-    setSelectedMovie(movie) {
-        this.setState({
-            selectedMovie: movie
+    getMovies(token) {
+        axios.get('https://mooflix.herokuapp.com/movies', {
+            headers: { Authorization: 'Bearer ${token}'}
+        })
+        .then(response => {
+            // Assign the result to the state
+            this.setState({
+                movies: response.data
+            });
+        })
+        .catch(function (error) {
+            console.log(error);
         });
     }
 
-    onLoggedIn(user) {
+    // setSelectedMovie(movie) {
+    //     this.setState({
+    //         selectedMovie: movie
+    //     });
+    // }
+
+    onLoggedIn(authData) {
+        console.log(authData);
         this.setState({
-            user
+            user: authData.user.Username
         });
+
+        localStorage.setItem('token', authData.token);
+        localStorage.setItem('user', authData.user.Username);
+        this.getMovies(authData.token);
     }
 
     onRegister(newRegistration) {
@@ -55,10 +75,14 @@ export class MainView extends React.Component {
     }
 
     render() {
-        const { movies, selectedMovie, user, newRegistration } = this.state;
+        const { movies, user, newRegistration } = this.state;
 
         // If there is no user, the LoginView is rendered. If there is a user logged in, the user details are *passed as a prop to the LoginView
-        if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />; 
+        if (!user) return <Row>
+            <Col>
+                <LoginView onLoggedIn={user => this.onLoggedIn(user)} />
+            </Col>
+        </Row>
 
         if (!newRegistration) return <RegistrationView onRegister={(newRegistration) => this.onRegister(newRegistration)} />;
 
@@ -66,20 +90,31 @@ export class MainView extends React.Component {
         if (movies.length === 0) return <div className="main-view" />;
 
         return (
-            <Row className="main-view" justify-content-md-center>
-                {selectedMovie
-                    ? (
-                        <Col md={8}>
-                            <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
+            <Router>
+                <Row className="main-view justify-content-md-center">
+                    <Route exact path="/" render={() => {
+                        if (!user) return <Col>
+                        <LoginView onLoggedIn={user => this.onLoggedIn(user)} />
                         </Col>
-                    )
-                    : movies.map(movie => (
-                        <Col md={3}>
-                            <MovieCard key={movie._id} movie={movie} onMovieClick={(newSelectedMovie) => { this.setSelectedMovie(newSelectedMovie); }}/>
+                         return movies.map(m => (
+                            <Col md={3} key={m._id}>
+                                <MovieCard movie={m} />
+                            </Col>
+                        ))
+                    }} />
+                    <Route path="/register" render={() => {
+                        return <Col>
+                            <RegistrationView />
                         </Col>
-                    ))
-                }
-            </Row>
+                    }} />
+                    <Route path="/movies/:movieId" render={({ match }) => {
+                        return <Col md={8}>
+                            <MovieView movie={movies.find(m => m._id === match.params.movieId)} />
+                         </Col>
+                    }} />
+
+                 </Row>
+            </Router>
         );
     }
 }
